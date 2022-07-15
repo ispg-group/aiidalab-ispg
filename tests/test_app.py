8317bffaa702b3c6d4c5b4cc4ce642f312d7fc11
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2022-07-15 23:48:14 +0100

    WIP: Test atmospec notebook

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,9 +2,10 @@
 from selenium.webdriver.common.by import By
 
 
-def test_example(selenium, url):
-    selenium.get(url("apps/apps/app/example.ipynb"))
+def test_atmospec(selenium, url):
+    #selenium.get(url("http://localhost:8100/apps/apps/quantum-espresso/qe.ipynb"))
+    selenium.get(url("apps/apps/aiidalab-ispg/atmospec.ipynb"))
     selenium.find_element(By.ID, "ipython-main-app")
     selenium.find_element(By.ID, "notebook-container")
     selenium.find_element(By.CLASS_NAME, "jupyter-widgets-view")
-    selenium.get_screenshot_as_file("screenshots/example.png")
+    selenium.get_screenshot_as_file("screenshots/atmospec.png")
