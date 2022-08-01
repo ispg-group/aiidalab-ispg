96422abd75353d16b143f1b429d876217f087414
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2022-08-01 01:03:07 +0100

    Remove aiidalab from reqs in setup.cfg
    Subsequent pip install would then install aiidalab
    in user environment which is undesirable.

--- aiidalab_ispg/widgets.py
+++ aiidalab_ispg/widgets.py
@@ -240,6 +240,7 @@ class WorkChainNodeViewerWidget(ipw.VBox):
 @register_viewer_widget("data.array.trajectory.TrajectoryData.")
 class TrajectoryDataViewer(StructureDataViewer):
 
+    # TODO: Do not subclass StructureDataViewer, but have it as a component
     trajectory = traitlets.Instance(Node, allow_none=True)
     _structures = []
     _energies = None
@@ -256,11 +257,11 @@ class TrajectoryDataViewer(StructureDataViewer):
 
         # Display energy if available
         self._energy = ipw.HTML(
-            value="Energy = ",
+            value="Energy ",
             placeholder="Energy",
         )
 
-        children = [self._step_selector, self._energy]
+        children = [ipw.HBox(children=[self._step_selector, self._energy])]
 
         super().__init__(
             children=children, configuration_tabs=["Selection", "Download"], **kwargs
