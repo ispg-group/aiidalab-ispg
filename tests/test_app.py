26fb41e93297e69d44a632a18e54ac283ebd72cd
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2022-12-02 18:56:59 +0000

    Bring back tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,47 +1,43 @@
-#!/usr/bin/env python
+import requests
 import time
 
-from selenium.webdriver.common.by import By
-
 # https://selenium-python.readthedocs.io/locating-elements.html
+from driver.webdriver.common.by import By
 
 
-def test_atmospec_app_init(selenium, url):
-    selenium.get(url("apps/apps/aiidalab-ispg/atmospec.ipynb"))
-    selenium.set_window_size(1920, 1450)
-    time.sleep(10)
-    selenium.get_screenshot_as_file("screenshots/atmospec-app.png")
-    return
-    selenium.find_element(By.ID, "ipython-main-app")
-    selenium.find_element(By.ID, "notebook-container")
-    selenium.find_element(By.CLASS_NAME, "jupyter-widgets-view")
+def test_notebook_service_available(notebook_service):
+    url, token = notebook_service
+    response = requests.get(f"{url}/?token={token}")
+    assert response.status_code == 200
 
 
-def test_atmospec_generate_mol_from_smiles(selenium, url):
-    selenium.get(url("apps/apps/aiidalab-ispg/atmospec.ipynb"))
-    # selenium.set_window_size(1920, 1000)
-    selenium.set_window_size(1920, 1450)
-    time.sleep(10)
-    selenium.get_screenshot_as_file("screenshots/atmospec-mol-generated.png")
-    return
-    smiles_textarea = selenium.find_element(By.XPATH, "//input[@placeholder='C=C']")
+def test_atmospec_app_init(selenium_driver):
+    driver = selenium_driver("atmospec.ipynb", wait_time=30.0)
+    driver.set_window_size(1920, 1450)
+    driver.get_screenshot_as_file("screenshots/atmospec-app.png")
+
+
+def test_atmospec_generate_mol_from_smiles(selenium_driver):
+    driver = selenium_driver("atmospec.ipynb", wait_time=30.0)
+    driver.set_window_size(1920, 1450)
+    smiles_textarea = driver.find_element(By.XPATH, "//input[@placeholder='C=C']")
     smiles_textarea.send_keys("C")
-    generate_mol_button = selenium.find_element(
+    generate_mol_button = driver.find_element(
         By.XPATH, "//button[contains(.,'Generate molecule')]"
     )
     generate_mol_button.click()
 
     # Once the structure is generated, proceed to the next workflow step
     time.sleep(2)
-    selenium.get_screenshot_as_file("screenshots/atmospec-mol-generated.png")
+    driver.get_screenshot_as_file("screenshots/atmospec-mol-generated.png")
 
-    confirm_btn = selenium.find_element(By.XPATH, "//button[contains(.,'Confirm')]")
+    confirm_btn = driver.find_element(By.XPATH, "//button[contains(.,'Confirm')]")
     confirm_btn.click()
-    selenium.get_screenshot_as_file("screenshots/atmospec-mol-confirmed.png")
+    driver.get_screenshot_as_file("screenshots/atmospec-mol-confirmed.png")
 
     # Test that we have indeed proceeded to the next step
-    selenium.find_element(By.XPATH, "//span[contains(.,'✓ Step 1')]")
+    driver.find_element(By.XPATH, "//span[contains(.,'✓ Step 1')]")
 
     # Note, the element is found even if it is hidden behind fold
     # Can't actually click submit, obviously
-    selenium.find_element(By.XPATH, "//button[contains(.,'Submit')]")
+    driver.find_element(By.XPATH, "//button[contains(.,'Submit')]")
