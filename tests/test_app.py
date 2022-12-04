9ff32a48385617114dcd62785caa181f3583c613
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2022-12-04 02:05:06 +0000

    Reenable test

--- tests/test_app.py
+++ tests/test_app.py
@@ -57,7 +57,7 @@ def test_spectrum_app_init(selenium_driver, screenshot_dir):
 def test_atmospec_app_init(selenium_driver, screenshot_dir):
     driver = selenium_driver("atmospec.ipynb", wait_time=30.0)
     driver.set_window_size(1920, 1450)
-    time.sleep(10)
+    driver.find_element(By.XPATH, "//button[contains(.,'Refresh')]")
     driver.get_screenshot_as_file(f"{screenshot_dir}/atmospec-app.png")
 
 
@@ -66,9 +66,6 @@ def test_atmospec_steps(selenium_driver, screenshot_dir):
     driver.set_window_size(1920, 2050)
 
     # For some reason this test is stuck on the loading page
-    time.sleep(10)
-    driver.get_screenshot_as_file(f"{screenshot_dir}/atmospec-steps-loading.png")
-    return
     smiles_textarea = driver.find_element(By.XPATH, "//input[@placeholder='C=C']")
 
     smiles_textarea.send_keys("C")
@@ -78,16 +75,12 @@ def test_atmospec_steps(selenium_driver, screenshot_dir):
     generate_mol_button.click()
 
     # Once the structure is generated, proceed to the next workflow step
-    time.sleep(2)
+    time.sleep(5)
     driver.get_screenshot_as_file(f"{screenshot_dir}/atmospec-mol-generated.png")
 
     confirm_btn = driver.find_element(By.XPATH, "//button[contains(.,'Confirm')]")
     confirm_btn.click()
-    driver.get_screenshot_as_file(f"{screenshot_dir}/atmospec-mol-confirmed.png")
-
     # Test that we have indeed proceeded to the next step
     driver.find_element(By.XPATH, "//span[contains(.,'✓ Step 1')]")
 
-    # Note, the element is found even if it is hidden behind fold
-    # Can't actually click submit, obviously
-    driver.find_element(By.XPATH, "//button[contains(.,'Submit')]")
+    driver.get_screenshot_as_file(f"{screenshot_dir}/atmospec-mol-confirmed.png")
