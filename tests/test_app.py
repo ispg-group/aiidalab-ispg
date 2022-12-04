da4a08b2091c5ac167d89475a33be66995e89db1
Daniel Hollas <danekhollas@gmail.com>
2022-12-04 13:22:07 +0000

    Decrease window width

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,6 +5,7 @@ import pytest
 # https://selenium-python.readthedocs.io/locating-elements.html
 from selenium.webdriver.common.by import By
 
+WINDOW_WIDTH = 1500
 
 @pytest.mark.tryfirst
 def test_post_install(notebook_service, docker_exec):
@@ -23,14 +24,14 @@ def test_dependencies(notebook_service, docker_exec):
 
 def test_conformer_generation_init(selenium_driver, screenshot_dir):
     driver = selenium_driver("conformer_generation.ipynb", wait_time=30.0)
-    driver.set_window_size(1920, 1450)
+    driver.set_window_size(WINDOW_WIDTH, 1450)
     driver.find_element(By.XPATH, "//button[contains(.,'Generate molecule')]")
     driver.get_screenshot_as_file(f"{screenshot_dir}/conformer-generation-init.png")
 
 
 def test_conformer_generation_steps(selenium_driver, screenshot_dir):
     driver = selenium_driver("conformer_generation.ipynb", wait_time=30.0)
-    driver.set_window_size(1920, 1450)
+    driver.set_window_size(WINDOW_WIDTH, 1450)
 
     # Generate methane molecule
     smiles_textarea = driver.find_element(By.XPATH, "//input[@placeholder='C=C']")
@@ -55,21 +56,21 @@ def test_conformer_generation_steps(selenium_driver, screenshot_dir):
 
 def test_spectrum_app_init(selenium_driver, screenshot_dir):
     driver = selenium_driver("spectrum_widget.ipynb", wait_time=30.0)
-    driver.set_window_size(1920, 1450)
+    driver.set_window_size(WINDOW_WIDTH, 1450)
     driver.find_element(By.XPATH, "//button[contains(.,'Download spectrum')]")
     driver.get_screenshot_as_file(f"{screenshot_dir}/spectrum-widget.png")
 
 
 def test_atmospec_app_init(selenium_driver, screenshot_dir):
     driver = selenium_driver("atmospec.ipynb", wait_time=30.0)
-    driver.set_window_size(1920, 1450)
+    driver.set_window_size(WINDOW_WIDTH, 1450)
     driver.find_element(By.XPATH, "//button[contains(.,'Refresh')]")
     driver.get_screenshot_as_file(f"{screenshot_dir}/atmospec-app.png")
 
 
 def test_atmospec_steps(selenium_driver, screenshot_dir):
     driver = selenium_driver("atmospec.ipynb", wait_time=40.0)
-    driver.set_window_size(1920, 1450)
+    driver.set_window_size(WINDOW_WIDTH, 1450)
 
     # For some reason this test is stuck on the loading page
     smiles_textarea = driver.find_element(By.XPATH, "//input[@placeholder='C=C']")
