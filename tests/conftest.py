decafb54a0d744a8fbe87b7c4812036b4e520cda
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2023-01-02 23:40:48 +0000

    Revert "Fix test"
    This reverts commit ff3751af00f216a04bd8ec9c23e06a8ce5853da8.

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,13 +9,7 @@ from requests.exceptions import ConnectionError
 from selenium.webdriver.common.by import By
 
 
-def is_notebook_responsive(ip, port):
-    return any(
-        is_url_responsive(f"{protocol}://{ip}:{port}") for protocol in ("http", "https")
-    )
-
-
-def is_url_responsive(url):
+def is_responsive(url):
     try:
         response = requests.get(url)
         if response.status_code == 200:
@@ -67,9 +61,10 @@ def notebook_service(docker_ip, docker_services, aiidalab_exec, nb_user, appdir)
 
     # `port_for` takes a container port and returns the corresponding host port
     port = docker_services.port_for("aiidalab", 8888)
+    url = f"http://{docker_ip}:{port}"
     token = os.environ["JUPYTER_TOKEN"]
     docker_services.wait_until_responsive(
-        timeout=30.0, pause=0.2, check=lambda: is_notebook_responsive(docker_ip, port)
+        timeout=30.0, pause=0.1, check=lambda: is_responsive(url)
     )
     return url, token
 
