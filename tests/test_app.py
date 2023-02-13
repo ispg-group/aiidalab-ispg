12ed4c5b9978dbe66e8024c1d7990f0271690bd9
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2023-02-13 15:51:22 +0000

    Fix get_attribute

--- tests/test_app.py
+++ tests/test_app.py
@@ -65,7 +65,7 @@ def test_conformer_generation_init(selenium_driver, final_screenshot):
     driver = selenium_driver("conformer_generation.ipynb", wait_time=30.0)
     driver.set_window_size(WINDOW_WIDTH, WINDOW_HEIGHT)
     btn = driver.find_element(By.XPATH, "//button[text()='Generate molecule']")
-    print(btn.getAttribute("disabled"))
+    print(btn.get_attribute("disabled"))
 
 
 def test_conformer_generation_steps(
@@ -146,9 +146,9 @@ def test_atmospec_steps(
     confirm = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.XPATH, "//button[text()='Confirm']"))
     )
-    print("Confirm button before click", btn.getAttribute("disabled"))
+    print("Confirm button before click", confirm.get_attribute("disabled"))
     confirm.click()
-    print("Confirm button after click", btn.getAttribute("disabled"))
+    print("Confirm button after click", confirm.get_attribute("disabled"))
 
     check_step_status(1, StepState.SUCCESS)
     check_step_status(2, StepState.CONFIGURED)
