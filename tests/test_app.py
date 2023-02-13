39e836ba3486c6676e6567f69483d9bd0759d3b4
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2023-02-13 12:52:36 +0000

    Check status fixture

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
 import requests
+from enum import Enum
 from pathlib import Path
 
 import pytest
@@ -10,6 +11,39 @@ from selenium.webdriver.common.by import By
 
 WINDOW_WIDTH = 1400
 WINDOW_HEIGHT = 1250
+
+# Copied over from aiidalab_widgets_base/wizard.py
+class StepState(Enum):
+    """Wizzard step state"""
+
+    INIT = 0  # the step is initialized and all widgets are typically disabled
+    CONFIGURED = 1  # configuration is valid
+    READY = 2  # step is ready for user input
+    ACTIVE = 3  # step is carrying out a runtime operation
+    SUCCESS = 4  # step has successfully completed
+    FAIL = -1  # the step has unrecoverably failed
+
+
+@pytest.fixture
+def check_step_status(driver):
+    ICONS = {
+        StepState.INIT: "○",
+        StepState.READY: "◎",
+        StepState.CONFIGURED: "●",
+        StepState.SUCCESS: "✓",
+        StepState.FAIL: "×",
+        # The ACTIVE state is "animated", see aiidalab_widgets_base/wizard.py,
+        # hence we cannot use it in tests.
+        # WizardAppWidgetStep.State.ACTIVE: ["\u25dc", "\u25dd", "\u25de", "\u25df"],
+    }
+
+    def _check_step_status(step_num, expected_state: StepState):
+        icon = ICONS[expected_state]
+        driver.find_element(
+            By.XPATH, f"//span[starts-with(.,'{icon} Step {step_num}')]"
+        )
+
+    return _check_step_status
 
 
 @pytest.mark.tryfirst
@@ -74,13 +108,13 @@ def test_spectrum_app_init(selenium_driver, final_screenshot):
     driver.find_element(By.XPATH, "//button[text()='Download spectrum']")
 
 
-def test_atmospec_app_init(selenium_driver, final_screenshot):
+def test_atmospec_app_init(selenium_driver, final_screenshot, check_step_status):
     driver = selenium_driver("atmospec.ipynb", wait_time=30.0)
     driver.set_window_size(WINDOW_WIDTH, WINDOW_HEIGHT)
-    driver.find_element(By.XPATH, "//span[starts-with(.,'◎ Step 1')]")
-    driver.find_element(By.XPATH, "//span[starts-with(.,'○ Step 2')]")
-    driver.find_element(By.XPATH, "//span[starts-with(.,'○ Step 3')]")
-    driver.find_element(By.XPATH, "//span[starts-with(.,'○ Step 4')]")
+    check_step_status(1, StepState.READY)
+    check_step_status(2, StepState.INIT)
+    check_step_status(3, StepState.INIT)
+    check_step_status(4, StepState.INIT)
     driver.find_element(By.XPATH, "//button[text()='Refresh']")
 
 
@@ -90,25 +124,27 @@ def test_atmospec_steps(
     final_screenshot,
     generate_mol_from_smiles,
     check_first_atom,
+    check_step_status,
 ):
     driver = selenium_driver("atmospec.ipynb", wait_time=40.0)
     driver.set_window_size(WINDOW_WIDTH, WINDOW_HEIGHT)
 
-    driver.find_element(By.XPATH, "//span[starts-with(.,'◎ Step 1')]")
+    check_step_status(1, StepState.READY)
+
     # Generate methane molecule
     generate_mol_from_smiles(driver, "C")
     check_first_atom(driver, "C")
     driver.get_screenshot_as_file(
         Path.joinpath(screenshot_dir, "atmospec-steps-mol-generated.png")
     )
+    check_step_status(1, StepState.CONFIGURED)
 
     confirm = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.XPATH, "//button[text()='Confirm']"))
     )
     confirm.click()
-    # Test that we have indeed proceeded to the next step
-    # TODO: Abstract these check in conftest
-    driver.find_element(By.XPATH, "//span[contains(.,'✓ Step 1')]")
-    driver.find_element(By.XPATH, "//span[starts-with(.,'● Step 2')]")
-    driver.find_element(By.XPATH, "//span[starts-with(.,'○ Step 3')]")
-    driver.find_element(By.XPATH, "//span[starts-with(.,'○ Step 4')]")
+
+    check_step_status(1, StepState.SUCCESS)
+    check_step_status(2, StepState.CONFIGURED)
+    check_step_status(3, StepState.INIT)
+    check_step_status(4, StepState.INIT)
