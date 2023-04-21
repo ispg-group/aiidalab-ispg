9071d8c59f543bed64281582cb1954fc5181fb48
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2023-04-21 15:10:49 +0000

    Working version

--- workflows/aiidalab_atmospec_workchain/optimization.py
+++ workflows/aiidalab_atmospec_workchain/optimization.py
@@ -1,6 +1,7 @@
 # AiiDA workflows dealing with optimization of molecules.
 
 import math
+import numpy as np
 
 from aiida.engine import WorkChain, calcfunction
 from aiida.engine import (
@@ -29,18 +30,31 @@ EVtoKJ = AUtoKCAL * KCALtoKJ / AUtoEV
 @calcfunction
 def structures_to_trajectory(arrays: Array = None, **structures) -> TrajectoryData:
     """Concatenate a list of StructureData to TrajectoryData
+
     Optionally, set additional data as Arrays.
     """
-    traj = TrajectoryData([structure for structure in structures.values()])
-    if arrays is not None:
-        for name in arrays.get_arraynames():
-            traj.set_array(name, arrays.get_array(name))
+    traj = TrajectoryData(list(structures.values()))
+    if arrays is None:
+        return traj
+
+    for name in arrays.get_arraynames():
+        traj.set_array(name, arrays.get_array(name))
+    # Copy over extras as well, except for private ones like '_aiida_hash'
+    extras = arrays.base.extras.all
+    for key in list(extras.keys()):
+        if key.startswith("_"):
+            del extras[key]
+    traj.base.extras.set_many(extras)
     return traj
 
 
-# NOTE: Should this be a calcfunction? But we cannot call calcfunction within calcfunction,
-# we'd need to create a workfunction.
 def calc_boltzmann_weights(energies: list, T: float):
+    """Compute Boltzmann weights for a list of energies.
+
+    param energies: list of energies / kJ per mole
+    param T: temperature / Kelvin
+    returns: Boltzmann weights as numpy array
+    """
     # Molar gas constant, Avogadro times Boltzmann
     R = 8.3144598
     RT = R * T
@@ -57,34 +71,38 @@ def extract_trajectory_arrays(**orca_output_parameters) -> Array:
 
     Return Array node, which will be appended to TrajectoryData node.
     """
-    gibbs_energies = [params["freeenergy"] for params in orca_output_parameters]
-    temperature = orca_output_parameters[0]["temperature"]
+    gibbs_energies = np.array(
+        [params["freeenergy"] for params in orca_output_parameters.values()]
+    )
     en0 = min(gibbs_energies)
-    relative_gibbs_energies_ev = [AUtoEV * (en - en0) for en in gibbs_energies]
-    # TODO: Verify units
-    boltzmann_weights = calc_boltzmann_weights(gibbs_energies, temperature)
+    relative_gibbs_energies_kj = AUtoKJ * (gibbs_energies - en0)
+
+    temperature = list(orca_output_parameters.values())[0]["temperature"]
+
+    boltzmann_weights = calc_boltzmann_weights(relative_gibbs_energies_kj, temperature)
+
     en = Array()
     en.set_array("gibbs_energies_au", gibbs_energies)
-    en.set_array("relative_gibbs_energies_ev", relative_gibbs_energies_ev)
+    en.set_array("relative_gibbs_energies_kj", relative_gibbs_energies_kj)
     en.set_array("boltzmann_weights", boltzmann_weights)
-    en.set_extra("temperature", orca_output_parameters[0]["temperature"])
+    en.set_extra("temperature", temperature)
 
     # For the TrajectoryData viewer compatibility
-    en.set_array("energies", relative_gibbs_energies_ev)
-    # TODO: Use kJ per mole
-    en.set_extra("energy_units", "eV")
+    en.set_array("energies", relative_gibbs_energies_kj)
+    en.set_extra("energy_units", "kJ/mole")
     return en
 
 
 # TODO: For now this is just a plain optimization,
 # the "robust" part needs to be implemented
 class RobustOptimizationWorkChain(OrcaBaseWorkChain):
-    """Molecular geometry optimization WorkChain that automatically
-    detects imaginary frequencies and restarts the optimization
-    until a true minimum is found.
+    """Molecular geometry optimization workflow.
+
+    The workflow automatically detects imaginary frequencies
+    and restarts the optimization until a true minimum is found.
     """
 
-    def _build_process_label(self):
+    def _build_process_label(self) -> str:
         return "Robust Optimization"
 
     @classmethod
@@ -127,14 +145,15 @@ class ConformerOptimizationWorkChain(WorkChain):
     to support optimization of multiple conformers in parallel.
     """
 
-    def _build_process_label(self):
+    def _build_process_label(self) -> str:
         return "Conformer Optimization Workflow"
 
     @classmethod
     def define(cls, spec):
         super().define(spec)
         spec.expose_inputs(RobustOptimizationWorkChain, exclude=["orca.structure"])
-        spec.input("structure", valid_type=(StructureData, TrajectoryData))
+        # TODO: Rename to trajectory?
+        spec.input("structure", valid_type=TrajectoryData)
 
         spec.output(
             "relaxed_structures",
@@ -154,13 +173,6 @@ class ConformerOptimizationWorkChain(WorkChain):
 
     def launch_conformer_optimization(self):
         inputs = self.exposed_inputs(RobustOptimizationWorkChain, agglomerate=False)
-        # Single conformer
-        # TODO: Test this!
-        if isinstance(self.inputs.structure, StructureData):
-            self.report("Launching Optimization for 1 conformer")
-            inputs.orca.structure = self.inputs.structure
-            return ToContext(confs=self.submit(RobustOptimizationWorkChain, **inputs))
-
         nconf = len(self.inputs.structure.get_stepids())
         self.report(f"Launching optimization for {nconf} conformers")
         for conf_id in self.inputs.structure.get_stepids():
@@ -172,33 +184,22 @@ class ConformerOptimizationWorkChain(WorkChain):
     def inspect_conformer_optimization(self):
         """Check whether all optimizations succeeded"""
         # TODO: Specialize errors. Can we expose errors from child workflows?
-        if isinstance(self.inputs.structure, StructureData):
-            if not self.ctx.confs.is_finished_ok:
-                return self.exit_codes.CONFORMER_ERROR
-            return
         for wc in self.ctx.confs:
             if not wc.is_finished_ok:
                 return self.exit_codes.CONFORMER_ERROR
 
     def collect_optimized_conformers(self):
         """Combine all optimized geometries into single TrajectoryData"""
-        # TODO: Include energies in TrajectoryData for optimized structures
-        # TODO: Calculate Boltzmann weights and append them to TrajectoryData
-        if isinstance(self.inputs.structure, StructureData):
-            relaxed_structures = {"struct_0": self.ctx.confs.outputs.relaxed_structure}
-            orca_output_params = {
-                "output_params_0": self.ctx.confs.outputs.output_parameters
-            }
-        else:
-            relaxed_structures = {
-                f"struct_{i}": wc.outputs.relaxed_structure
-                for i, wc in enumerate(self.ctx.confs)
-            }
-            # TODO: Verify everything is done in the same order
-            orca_output_params = {
-                f"output_params_{i}": wc.outputs.output_parameters
-                for i, wc in enumerate(self.ctx.confs)
-            }
-        array_data = extract_trajectory_arrays(**orca_output_params)
+        # TODO: Switch to lists in AiiDA 2.3
+        relaxed_structures = {}
+        orca_output_params = {}
+        for wc in self.ctx.confs:
+            relaxed_structures[f"struct_{wc.pk}"] = wc.outputs.relaxed_structure
+            orca_output_params[f"params_{wc.pk}"] = wc.outputs.output_parameters
+
+        array_data = None
+        if len(self.ctx.confs) > 1:
+            array_data = extract_trajectory_arrays(**orca_output_params)
+
         trajectory = structures_to_trajectory(arrays=array_data, **relaxed_structures)
         self.out("relaxed_structures", trajectory)
