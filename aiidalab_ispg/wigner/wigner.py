dd5ab3ae79a68484119cfe1947ceb867e88f422d
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-02-05 01:37:47 +0000

    Basic Wigner CLI (#231)
    WIP: Wigner sampling script using cclib to parse QM output files.

--- aiidalab_ispg/wigner/wigner.py
+++ aiidalab_ispg/wigner/wigner.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+# Specification of standalone script dependencies according to PEP 723
+# /// script
+# dependencies = ['ase', 'tdqm', 'cclib>=1.8']
+# ///
+
 # Script for the calculation of Wigner distributions in coordinate space
 import copy
 import math
@@ -143,3 +148,126 @@ class Wigner:
         oscillator and P contains the corresponding momentum.
         The function returns a probability for this set of parameters."""
         return (math.exp(-(Q**2)) * math.exp(-(P**2)), 0.0)
+
+
+# Below are functions for CLI standalone use
+def parse_cmd():
+    import argparse
+
+    desc = "Program for harmonic Wigner sampling"
+    prog = "harmonwig"
+    parser = argparse.ArgumentParser(description=desc, prog=prog)
+    parser.add_argument(
+        "input_file", metavar="INPUT_FILE", help="Output file from ab initio program."
+    )
+    parser.add_argument(
+        "-n",
+        "--nsamples",
+        type=int,
+        default=1,
+        help="Number of Wigner samples",
+    )
+    parser.add_argument(
+        "--seed",
+        dest="seed",
+        type=int,
+        default=42424242,
+        help="Random seed",
+    )
+    parser.add_argument(
+        "--freqthr",
+        dest="low_freq_thr",
+        default=0.0,
+        type=float,
+        help="Low-frequency threshold",
+    )
+
+    parser.add_argument(
+        "--file-format",
+        dest="file_fmt",
+        default="auto",
+        help="Format of the output file",
+    )
+
+    parser.add_argument(
+        "-o",
+        "--output-file",
+        dest="output_fname",
+        default="harmonic_samples.xyz",
+        help="Output file name",
+    )
+
+    return parser.parse_args()
+
+
+def error(msg: str):
+    import sys
+
+    print(f"ERROR: {msg}")
+    sys.exit(1)
+
+
+def read_qm_output(fname: str, fmt: str = "auto") -> dict:
+    from pathlib import Path
+
+    from cclib.io import ccread
+
+    path = Path(fname)
+    try:
+        with path.open("r") as f:
+            parsed_obj = ccread(f)
+    except FileNotFoundError as e:
+        error(str(e))
+
+    if parsed_obj is None:
+        error("Could not read ORCA output")
+    d = parsed_obj.getattributes()
+    return d
+
+
+def validate(out: dict):
+    if not out["optdone"]:
+        error("Optimization did not finish!")
+    req_keys = ["atomcoords", "atommasses", "vibdisps", "vibfreqs"]
+    for key in req_keys:
+        if key not in out:
+            error("Could not find frequency data in ORCA output file")
+
+
+if __name__ == "__main__":
+    opts = parse_cmd()
+
+    import ase
+    from tqdm import tqdm
+
+    out = read_qm_output(opts.input_file, fmt=opts.file_fmt)
+    # We assume that the last coordinates are the optimized ones
+    coords = out["atomcoords"][0]
+    ase_mol = ase.Atoms(
+        numbers=out["atomnos"], positions=coords, masses=out["atommasses"], pbc=False
+    )
+
+    print("Normal mode frequencies [cm^-1]:")
+    print(out["vibfreqs"])
+    print("Atom masses:")
+    print(out["atommasses"])
+
+    wigner = Wigner(
+        ase_mol,
+        out["vibfreqs"],
+        out["vibdisps"],
+        seed=opts.seed,
+        low_freq_thr=opts.low_freq_thr,
+    )
+
+    print(f"Generating {opts.nsamples} samples to {opts.output_fname}")
+    barfmt = "{l_bar}{bar}|{n_fmt}/{total_fmt}    "
+    wigner_samples = []
+    for _ in tqdm(range(opts.nsamples), delay=0.3, colour="green", bar_format=barfmt):
+        wigner_samples.append(wigner.get_ase_sample())
+
+    ase.io.write(
+        opts.output_fname,
+        images=wigner_samples,
+        format="extxyz",
+    )
