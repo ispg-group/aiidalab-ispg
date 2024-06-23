fc6dd92255612cb50c0970379f095abc02e0c0af
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-06-24 00:21:51 +0200

    Update CI (#243)
    * Don't install firefox manually
    * Don't install geckodriver manually
    * Pin full-stack image
    * Increase timeouts
    * Update test dependencies

--- tests/app/conftest.py
+++ tests/app/conftest.py
@@ -72,14 +72,14 @@ def notebook_service(docker_ip, docker_services, aiidalab_exec, nb_user, appdir)
     url = f"http://{docker_ip}:{port}"
     token = os.environ["JUPYTER_TOKEN"]
     docker_services.wait_until_responsive(
-        timeout=30.0, pause=0.1, check=lambda: is_responsive(url)
+        timeout=30.0, pause=0.5, check=lambda: is_responsive(url)
     )
     return url, token
 
 
 @pytest.fixture()
 def selenium_driver(selenium, notebook_service):
-    def _selenium_driver(nb_path, wait_time=5.0):
+    def _selenium_driver(nb_path, wait_time=30.0):
         url, token = notebook_service
         url_with_token = urljoin(
             url, f"apps/apps/aiidalab-ispg/notebooks/{nb_path}?token={token}"
@@ -90,7 +90,7 @@ def selenium_driver(selenium, notebook_service):
         selenium.find_element(By.ID, "ipython-main-app")
         selenium.find_element(By.ID, "notebook-container")
         selenium.find_element(By.CLASS_NAME, "jupyter-widgets-view")
-        WebDriverWait(selenium, 20).until(
+        WebDriverWait(selenium, timeout=20, poll_frequency=0.5).until(
             EC.invisibility_of_element((By.ID, "appmode-busy"))
         )
         return selenium
@@ -109,7 +109,10 @@ def generate_mol_from_smiles(selenium):
                 (By.XPATH, "//button[text()='Generate molecule']")
             )
         ).click()
-        time.sleep(3)
+        WebDriverWait(selenium, timeout=20, poll_frequency=0.5).until(
+            EC.invisibility_of_element((By.ID, "appmode-busy"))
+        )
+        time.sleep(3.0)
 
     return _generate_mol
 
